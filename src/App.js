ba50666aaa2415d7d09100077af7cc2ab8c1d11b
thangvv <70836195+thangvd2312@users.noreply.github.com>
2022-06-21 10:18:41 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,33 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: {
+        firstName: "Vũ Văn",
+        lastName: "Thắng"
+      },
+      company: "FPT Software"
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello, My name is ThangVV</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p>
+            Hello, My name is {this.state.name.firstName}{" "}
+            {this.state.name.lastName}, I work at {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState({ name: { firstName: "Thang", lastName: "VV6" } });
+              console.log(this.state);
+            }}
           >
-            Learn React
-          </a>
+            Change Name
+          </button>
         </header>
       </div>
     );
