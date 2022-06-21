d9fb94243fbd7d3ac9fbb7d62a14b289eae62cd8
thangvv <70836195+thangvd2312@users.noreply.github.com>
2022-06-21 09:52:08 +0700

    Class Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import "./styles.css";
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello, My name is ThangVV</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
+export default App;
