ac77ccd00f1fb147448c143a44acae77fce6c59b
thangvv <70836195+thangvd2312@users.noreply.github.com>
2022-06-22 13:07:54 +0700

    Optimizations Filter

--- src/App.js
+++ src/App.js
@@ -23,10 +23,21 @@ class App extends Component {
         );
       });
   }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return {
+        searchField
+      };
+    });
+  };
   render() {
     console.log("render");
-    const filteredMonsters = this.state.monters.filter((monter) => {
-      return monter.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monters.filter((monter) => {
+      return monter.name.toLocaleLowerCase().includes(searchField);
     });
     return (
       <div className="App">
@@ -34,14 +45,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-            this.setState(() => {
-              return {
-                searchField
-              };
-            });
-          }}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monter) => {
           return (
